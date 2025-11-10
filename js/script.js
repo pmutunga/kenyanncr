bf17c533ebe91085eef6c13dc2fa29ce28abbae6
Paskwa <paskwa.mutunga@ipsafari.com>
2025-11-10 17:55:49 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Basic form submission handler (placeholder)
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('Thank you for registering! Your details have been received.');
+});
